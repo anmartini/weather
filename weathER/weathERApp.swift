3f83613a1d1e25b4c1019407fc2addb8e652f681
Andrea Martini <andrea@anmartini.it>
2021-02-25 17:02:18 +0100

    Initial Commit

---
+++ weathER/weathERApp.swift
@@ -0,0 +1,17 @@
+//
+//  weathERApp.swift
+//  weathER
+//
+//  Created by Andrea Martini on 25/02/21.
+//
+
+import SwiftUI
+
+@main
+struct weathERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
